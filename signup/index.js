cf0e7e4210379a5e76e6de35490315f5035b7576
utkarsh <utkarsh382004@gmail.com>
2023-05-17 00:10:57 +0530

    Singup Form Done

--- signup/index.js
+++ signup/index.js
@@ -1,26 +1,53 @@
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const form = document.getElementsByTagName("form")[0];
+const message = document.getElementById('message');
+
+
+const displayError = (mess) =>{
+    message.style.display = "block";
+    message.innerHTML = mess
+}
+
+const validateEmail = (value) =>{
+    const emailPattern = /^.+@[a-z]{2,6}.(com|in)$/
+    if(!emailPattern.test(value)){
+        email.value = "";
+        return false;
+    }
+    return true;
+}
 
 const fetchByThen = () =>{
     const credentials = {
         "email":email.value,
         "password":password.value
     }
-    fetch("https://reqres.in/api/login" ,  {
+    const options = {
         method:"POST",
         body : JSON.stringify(credentials),
         headers:{
             'Content-type': 'application/json'
-        }
-    })
-        .then((response) => response.json())
+        } 
+    }
+    if(validateEmail(email.value)){
+    fetch("https://reqres.in/api/login" ,  options)
+        .then((response) =>{  
+            if(!response.ok) return false
+            return response.json();
+        })
         .then((value)=>{
-            console.log(value);
+            if(!value) throw Error("The login information is not correct")
+            console.log("yaya")
         })
-        .catch((error) => {
-            console.log(error);
+        .catch((err) => {
+            
         })
+    }
+}
+
+const fetchByAwait =async () =>{
+
 }
 
 form.addEventListener('submit' ,(event)=>{
