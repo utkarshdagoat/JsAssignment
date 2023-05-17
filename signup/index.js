2ae9de089be583fc874cac97e4baeff70c2aab02
utkarsh <utkarsh382004@gmail.com>
2023-05-17 17:57:41 +0530

    Singup Form Done

--- signup/index.js
+++ signup/index.js
@@ -2,13 +2,19 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const form = document.getElementsByTagName("form")[0];
 const message = document.getElementById('message');
+const successMessage = document.getElementById('successMessage');
 
 
 const displayError = (mess) =>{
     message.style.display = "block";
-    message.innerHTML = mess
+    message.innerHTML = mess;
+    email.value = ""
+    password.value = ""
 }
-
+const displaySuccess = (mess) =>{
+    successMessage.style.display = "block";
+    successMessage.innerHTML = mess;
+}
 const validateEmail = (value) =>{
     const emailPattern = /^.+@[a-z]{2,6}.(com|in)$/
     if(!emailPattern.test(value)){
@@ -37,11 +43,11 @@ const fetchByThen = () =>{
             return response.json();
         })
         .then((value)=>{
-            if(!value) throw Error("The login information is not correct")
-            console.log("yaya")
+            if(!value) throw Error("The login information is not correct");
+            displaySuccess("The form is submitted successfully");
         })
         .catch((err) => {
-            
+            displayError(err);
         })
     }
 }
