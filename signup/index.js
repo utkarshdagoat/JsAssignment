09d3e19bae28d03e2d3638aacdcb329f027bfc74
utkarsh <utkarsh382004@gmail.com>
2023-05-18 19:12:07 +0530

    Singup Form Done

--- signup/index.js
+++ signup/index.js
@@ -19,32 +19,36 @@ const validateEmail = (value) =>{
     const emailPattern = /^.+@[a-z]{2,6}.(com|in)$/
     if(!emailPattern.test(value)){
         email.value = "";
+        displayError("The Email is not valid")
         return false;
     }
     return true;
 }
 
-const fetchByThen = () =>{
+
+const fetchByThen = () =>{  
     const credentials = {
-        "email":email.value,
-        "password":password.value
+        email: email.value,
+        password: password.value
     }
     const options = {
         method:"POST",
         body : JSON.stringify(credentials),
         headers:{
-            'Content-type': 'application/json'
-        } 
+            'Content-type': 'application/json',
+            'Accept':'*/*'
+        }
     }
+    console.log(email.value);
+    console.log(password.value);
     if(validateEmail(email.value)){
     fetch("https://reqres.in/api/login" ,  options)
-        .then((response) =>{  
-            if(!response.ok) return false
+        .then((response) =>{ 
+            if(!response.ok) throw  Error("Invalid credentialss");
             return response.json();
         })
-        .then((value)=>{
-            if(!value) throw Error("The login information is not correct");
-            displaySuccess("The form is submitted successfully");
+        .then((res)=>{
+            displaySuccess(`The form is submitted successfully`);
         })
         .catch((err) => {
             displayError(err);
@@ -53,12 +57,34 @@ const fetchByThen = () =>{
 }
 
 const fetchByAwait =async () =>{
-
+    try{
+        const credentials = {
+            email: email.value,
+            password: password.value
+        }
+        const options = {
+            method:"POST",
+            body : JSON.stringify(credentials),
+            headers:{
+                'Content-type': 'application/json',
+                'Accept':'*/*'
+            }
+        }
+        if(validateEmail(email.value)){
+            const response = await fetch( "https://reqres.in/api/login" ,options);
+            const readableData = await response.json();
+            if(!response.ok) throw new Error(readableData.error);
+            displaySuccess("The form was submitted succesfully")
+        }
+    } catch(err){
+        displayError(err);
+    }
 }
 
 form.addEventListener('submit' ,(event)=>{
     event.preventDefault();
-    fetchByThen();
+    //fetchByThen();
+    fetchByAwait();
 })
 
 
